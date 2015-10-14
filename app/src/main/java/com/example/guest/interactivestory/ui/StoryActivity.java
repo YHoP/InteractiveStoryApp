88e03d43af81cee33e497a9c8b4e6551f57157ca
YHoP <ypeng.tw@gmail.com>
2015-10-14 12:02:42 -0700

    setText to mTextView, mChoice1 & 2

--- app/src/main/java/com/example/guest/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/example/guest/interactivestory/ui/StoryActivity.java
@@ -1,15 +1,27 @@
 package com.example.guest.interactivestory.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.guest.interactivestory.R;
+import com.example.guest.interactivestory.model.Page;
+import com.example.guest.interactivestory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
 
     // private Page[] mPages;
+    private Story mStory =  new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
 
     public static final String TAG = StoryActivity.class.getSimpleName();
 
@@ -24,9 +36,31 @@ public class StoryActivity extends AppCompatActivity {
             name = "Friend";
         }
 
-        Log.d(TAG, name);
-
+        Log.d(TAG, mName);
         // mPages = new Page[7];
+
+        mImageView = (ImageView)findViewById(R.id.storyImageView);
+        mTextView = (TextView)findViewById(R.id.storyTextView);
+        mChoice1 = (Button)findViewById(R.id.choiceButton1);
+        mChoice2 = (Button)findViewById(R.id.choiceButton2);
+        loadPage();
+    }
+
+    private void loadPage() {
+        Page page = mStory.getPage(0);
+        // Drawable drawable = getResources().getDrawable(page.getImageId());
+        // mImageView.setImageDrawable(drawable);
+
+        mImageView.setImageResource(page.getImageId());
+
+        String pageText = page.getText();
+
+        // Add the name if placeholder included. Won't add if no placeholder
+        pageText = String.format(pageText, mName);
+
+        mTextView.setText(page.getText());
+        mChoice1.setText(page.getChoice1().getText());
+        mChoice2.setText(page.getChoice2().getText());
 
     }
 }
